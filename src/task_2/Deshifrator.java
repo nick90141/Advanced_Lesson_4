dfd15ff8bbe425297cf9e20e1278aefc3d84c020
Mykyta <nick90141@gmail.com>
2023-09-19 15:59:24 +0300

    ЗМІНЕНЕ 2

--- src/task_2/Deshifrator.java
+++ src/task_2/Deshifrator.java
@@ -1,34 +1,15 @@
 package task_2;
 
 import java.io.BufferedReader;
-<<<<<<< HEAD
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-=======
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
->>>>>>> 1e8b940 (ЗМІНЕНЕ)
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Deshifrator {
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-        File file = new File("D:\\КУРС ПО JAVA\\Java Advanced\\№4. Regular expressions and Date API\\Homework_4\\src\\task_2\\text_file.txt");
-
-        try {
-            StringBuilder content = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-            reader.close();
-=======
         String relativeFilePath = "src/task_2/text_file.txt";
 
         try {
@@ -41,7 +22,6 @@ public class Deshifrator {
                     content.append(line).append("\n");
                 }
             }
->>>>>>> 1e8b940 (ЗМІНЕНЕ)
 
             String wordToReplace = "вокруг|под|ласково|среди|на|за|с|в";
             String replacementWord = "Java";
@@ -58,8 +38,4 @@ public class Deshifrator {
             e.printStackTrace();
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1e8b940 (ЗМІНЕНЕ)
